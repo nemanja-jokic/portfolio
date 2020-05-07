1414aec45594e4ff376134a2204625f7747223cf
nemanja-jokic <57893634+nemanja-jokic@users.noreply.github.com>
2020-05-07 11:35:04 +0200

    modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -7,23 +7,27 @@ export default class Modal extends React.Component {
     return (
       <div id="modal">
         <div className="modal-content">
-          <div className="modal-header">
-            <h3>GREETINGS</h3>
-            <Button id="close" onClick={this.props.removeModal} />
-          </div>
           <div className="modal-body">
-            <p>Ovo je nas modal</p>
+            <div className="modal-header">
+              <div></div>
+              <h3>GREETINGS</h3>
+              <Button id="close" onClick={this.props.removeModal} />
+            </div>
+            <p>My name is Nemanja</p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illo
-              eum nisi minima fugit, dignissimos soluta blanditiis eligendi non
-              obcaecati, a labore quas quisquam dolorum omnis natus consequuntur
-              expedita quaerat rem quos quae temporibus. Nesciunt tempore
-              repellat dicta, saepe obcaecati beatae accusamus facilis
-              asperiores officia magnam perferendis quo magni quasi?
+              I’m an actor. I'm driving in a van with my fellow workers from
+              school to school and performing puppet shows for children. I like
+              that job. But a year ago my situation has changed and I was forced
+              to stay almost all day at home. I was thinking: "What should I do
+              from now on?Which job can enable me to work from home?" So coding
+              was the first thing that came to my mind; but as I believed that
+              only really smart people can do that, I was a little bit scared
+              whether I'm smart enough. But I haven't had a choice. So I started
+              leaning to code on the internet...
             </p>
-          </div>
-          <div className="modal-footer">
-            <p>Ovo je footer modala</p>
+            <div className="modal-footer">
+              <p>This is my presentation</p>
+            </div>
           </div>
         </div>
       </div>
