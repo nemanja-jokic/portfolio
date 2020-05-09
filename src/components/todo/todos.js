25fea232ffcf987966b9c4562bf8e0a11215b37b
nemanja-jokic <57893634+nemanja-jokic@users.noreply.github.com>
2020-05-09 13:17:21 +0200

    todos style

--- src/components/todo/todos.js
+++ src/components/todo/todos.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React from "react";
+import Button from "../button.js";
 
-const Todos = ({todos, deleteTodo}) => {
-
-    const todoList = todos.length ? (
-        todos.map(todo => {
-            return (
-                <div className='collection-item' key={todo.id}>
-                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
-                </div>
-            )
-        })
-    ) : (
-        <p>You have no todo's left </p>
-    )
-    return (
-        <div className='todos collection'>
-            {todoList}
+const Todos = ({ todos, deleteTodo }) => {
+  const todoList = todos.length ? (
+    todos.map((todo) => {
+      return (
+        <div className="collection-item" key={todo.id}>
+          <span
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {todo.content}
+          </span>
+          {
+            <Button
+              tone="todo-button"
+              id={"X"}
+              onClick={() => {
+                deleteTodo(todo.id);
+              }}
+            />
+          }
         </div>
-    )
-}
+      );
+    })
+  ) : (
+    <p>You have no todo's left </p>
+  );
+  return <div className="todos collection">{todoList}</div>;
+};
 export default Todos;
